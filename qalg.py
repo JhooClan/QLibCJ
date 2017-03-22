d3394453bf235909d37d87edfd6f849836bbab93
Mowstyl <indikalesken@hotmail.com>
2017-03-22 04:19:02 -0400

    Added Teleportation Algorithm

--- qalg.py
+++ qalg.py
@@ -12,3 +12,26 @@ def Bal(n):
         t = np.copy(b[i,:])
         b[i], b[i+1] = b[i+1, :], t
     return b
+
+def Teleportation(qbit):
+    r = QRegistry([qbit, QZero(), QZero()])
+    print ("Original registry:\n", r.state)
+    r.ApplyGate(np.kron(I(1), np.kron(Hadamard(1), I(1))))
+    r.ApplyGate(np.kron(I(1), CNOT()))
+    print ("With Bell+ state:\n", r.state)
+    r.ApplyGate(np.kron(CNOT(), I(1)))
+    r.ApplyGate(np.kron(Hadamard(1), I(2)))
+    print ("\nBefore measurement:\n", r.state)
+    m = r.Measure([0,1])
+    print ("q0 = ", m[0], "\nq1 = ", m[1])
+    q0 = QZero()
+    q1 = QZero()
+    if (m[1] == 1):
+        q1 = QOne()
+        r.ApplyGate(np.kron(I(2), PauliX()))
+    if (m[0] == 1):
+        q0 = QZero()
+        r.ApplyGate(np.kron(I(2), PauliZ()))
+    er = QRegistry([q0, q1, qbit])
+    print ("\nExpected result:\n", er.state, "\nResult:\n", r.state)
+    return r
