e9981c68e07a376266e7899fe2c59935d887425e
Mowstyl <indikalesken@hotmail.com>
2018-05-07 16:35:45 +0200

    Improved factor2 method by using modular exponential

--- tests/jfactor.py
+++ tests/jfactor.py
@@ -1,13 +1,12 @@
 import time
 from fractions import gcd
 from random import randrange
-from functools import reduce
 
 def factor2(num): # Algoritmo Las Vegas que factoriza un numero N = p*q, p y q primos
     start = time.process_time()
     a = randrange(1, num) # Inicializo a con un valor en Gnum
     print ("choosen a = ", a)
-    p = reduce(gcd, (a, num)) # Compruebo si son coprimos. Si el resultado es distinto a 1 habremos obtenido p
+    p = gcd(a, num) # Compruebo si son coprimos. Si el resultado es distinto a 1 habremos obtenido p
     if (p != 1):
         print ("Time elapsed: ", (time.process_time() - start), " seconds")
         return (p, int(num/p))
@@ -17,13 +16,14 @@ def factor2(num): # Algoritmo Las Vegas que factoriza un numero N = p*q, p y q p
             print ("r = ", r)
             if r % 2 == 0: # Comprobamos si r es par
                 print ("r is even")
-                x = pow(a, int(r/2))
-                if (x + 1) % num != 0: # Comprobamos si x + 1 != 0 (mod p*q)
-                    p = reduce(gcd, (x - 1, num))
+                x = pow(a, int(r/2), num)
+                if x != num - 1: # Comprobamos si x + 1 = 0 (mod p*q)
+                    p = gcd(x - 1, num)
                     q = int(num/p)
                     print ("Time elapsed: ", (time.process_time() - start), " seconds")
                     return (p, q)
                 else:
+                    print ("x + 1 = 0 (Mod n)")
                     break
             else:
                 print ("r is odd")
